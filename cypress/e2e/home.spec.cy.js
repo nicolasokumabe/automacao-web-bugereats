e04a36a66b07e46c58af34af08a0ad227a5d6b26
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-11-30 20:19:27 -0300

    Sem Selector Playground

--- cypress/e2e/home.spec.cy.js
+++ cypress/e2e/home.spec.cy.js
@@ -7,6 +7,6 @@ describe('home page', () => {
     it('app deve estar online', () => {
       cy.viewport(1920, 1080)
       cy.visit('https://buger-eats.vercel.app')
-      cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+      cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
 })
